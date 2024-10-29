e7ed59c55c58f06681a327423cd46cdaef1af4ed
Baldev Singh <personalemailofbaldev@gmail.com>
2024-10-29 20:26:13 +0530

    29/10/24 19:10

--- ani-cli.bash
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-pkg update
-pkg install git
-git clone https://github.com/rofrol/termux-yt-dlp.git
-cd termux-yt-dlp
-bash install.sh
--- installer.bash
+++ installer.bash
@@ -6,10 +6,22 @@ termux-am
 
 termux-setup-storage
 
+pkg update
+
 pkg install fish starship yazi ffmpegthumbnailer p7zip jq poppler fd ripgrep fzf zoxide imagemagick ani-cli ncurses-utils unzip
 
 curl -fsSL https://raw.githubusercontent.com/arnavgr/termux-nf/main/install.sh | bash
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/CascadiaCode.zip
-unzip CascadiaCode
-getnf -i CaskaydiaCoveNerdFont-Bold.ttf CaskaydiaCoveNerdFont-BoldItalic.ttf CaskaydiaCoveNerdFont-ExtraLight.ttf CaskaydiaCoveNerdFont-ExtraLightItalic.ttf CaskaydiaCoveNerdFont-Italic.ttf CaskaydiaCoveNerdFont-Light.ttf CaskaydiaCoveNerdFont-LightItalic.ttf CaskaydiaCoveNerdFont-Regular.ttf CaskaydiaCoveNerdFont-SemiBold.ttf CaskaydiaCoveNerdFont-SemiBoldItalic.ttf CaskaydiaCoveNerdFont-SemiLight.ttf CaskaydiaCoveNerdFont-SemiLightItalic.ttf
-rm *.zip *.ttf LICENSE README.md
+
+mv ./font $HOME/
+
+chsh
+
+getnf
+
+git clone https://github.com/rofrol/termux-yt-dlp.git
+
+cd termux-yt-dlp
+
+bash install.sh
+
+rm termux-yt-dlp
