1ba1d7a9a929af579dbbbc4c46b47e468ecee909
Baldev Singh <personalemailofbaldev@gmail.com>
2024-10-29 14:55:18 +0530

    29/10/24 14:10

--- ani-cli.bash
+++ ani-cli.bash
@@ -3,4 +3,4 @@
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/CascadiaCode.zip
 unzip CascadiaCode
 getnf -rm CaskaydiaCoveNerdFont-Bold.ttf CaskaydiaCoveNerdFont-BoldItalic.ttf CaskaydiaCoveNerdFont-ExtraLight.ttf CaskaydiaCoveNerdFont-ExtraLightItalic.ttf CaskaydiaCoveNerdFont-Italic.ttf CaskaydiaCoveNerdFont-Light.ttf CaskaydiaCoveNerdFont-LightItalic.ttf CaskaydiaCoveNerdFont-Regular.ttf CaskaydiaCoveNerdFont-SemiBold.ttf CaskaydiaCoveNerdFont-SemiBoldItalic.ttf CaskaydiaCoveNerdFont-SemiLight.ttf CaskaydiaCoveNerdFont-SemiLightItalic.ttf
-rm *.tff LICENSE README.md
+rm *.zip *.ttf LICENSE README.md
