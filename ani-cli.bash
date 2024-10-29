e5dc15d295888175ad227132afc716c7ae7ab0fd
Baldev Singh <personalemailofbaldev@gmail.com>
2024-10-29 14:51:27 +0530

    29/10/24 14:10

--- ani-cli.bash
+++ ani-cli.bash
@@ -1,3 +1,6 @@
 #! /usr/bin/env bash
 
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/CascadiaCode.zip
+unzip CascadiaCode
+getnf -rm CaskaydiaCoveNerdFont-Bold.ttf CaskaydiaCoveNerdFont-BoldItalic.ttf CaskaydiaCoveNerdFont-ExtraLight.ttf CaskaydiaCoveNerdFont-ExtraLightItalic.ttf CaskaydiaCoveNerdFont-Italic.ttf CaskaydiaCoveNerdFont-Light.ttf CaskaydiaCoveNerdFont-LightItalic.ttf CaskaydiaCoveNerdFont-Regular.ttf CaskaydiaCoveNerdFont-SemiBold.ttf CaskaydiaCoveNerdFont-SemiBoldItalic.ttf CaskaydiaCoveNerdFont-SemiLight.ttf CaskaydiaCoveNerdFont-SemiLightItalic.ttf
+rm *.tff LICENSE README.md
--- installer.bash
+++ installer.bash
@@ -6,6 +6,6 @@ termux-am
 
 termux-setup-storage
 
-pkg install fish starship yazi ffmpegthumbnailer p7zip jq poppler fd ripgrep fzf zoxide imagemagick ani-cli ncurses-utils
+pkg install fish starship yazi ffmpegthumbnailer p7zip jq poppler fd ripgrep fzf zoxide imagemagick ani-cli ncurses-utils unzip
 
 curl -fsSL https://raw.githubusercontent.com/arnavgr/termux-nf/main/install.sh | bash
