ea88d2513f5c8122f635d03372346144c8b02bbf
Baldev Singh <personalemailofbaldev@gmail.com>
2024-10-29 09:28:07 +0530

    backup on 29/10/24

--- home-manager/fish.nix
+++ home-manager/fish.nix
@@ -4,16 +4,7 @@ _: {
     interactiveShellInit = ''
       set fish_greeting
     '';
-    loginShellInit = ''
-      if status is-login
-        if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-          Hyprland
-        end
-      end
-    '';
     shellAbbrs = {
-      xc = "nh clean all -k 3";
-      xs = "nh os switch";
       da = "ani-cli";
       dv = "yt-dlp";
     };
